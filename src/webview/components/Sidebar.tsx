431633d42e9dac7c0f4b962ac7aa2446dc89c608
whiteboxideas <dev@whiteboxideas.com>
2024-01-13 11:43:44 +0200

    Add comment to Sidebar component to test

--- src/webview/components/Sidebar.tsx
+++ src/webview/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {Flow} from './Flow';
 import {Navbar} from './Navbar';
 import { useSidebarLogic } from "./sidebar-parts/useSidebarLogic";
-
+ //lets see if this works
 export const Sidebar = () => {
   const {rootFile,initialNodes, initialEdges, nodeIDs,handleAllProps}: any =  useSidebarLogic()
   return (
